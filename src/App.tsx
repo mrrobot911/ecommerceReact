72c655c7bc8bf27d1ff650d51077d4d1039aebec
Alex <alex.devenrgy@gmail.com>
2024-05-01 01:38:33 +0300

    feat: add shadcn-ui and eslint set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Link } from 'react-router-dom';
+
+import { Button } from '@/components/ui/button';
+
 export default function App() {
-  return <h1 className="text-3xl font-bold text-red-500 underline">Hello world!</h1>;
+  return (
+    <div className='p-5'>
+      <h1 className='text-3xl mb-2 font-bold text-red-500 underline'>Hello world!</h1>
+      <div className='space-x-5'>
+        <Button asChild variant='outline'>
+          <Link to='/'>Login</Link>
+        </Button>
+        <Button variant='destructive'>Fuck you!</Button>
+      </div>
+    </div>
+  );
 }
