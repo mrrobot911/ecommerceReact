1888219123f9a5c327db83dbae27f6a97636d1ff
Alex <alex.devenrgy@gmail.com>
2024-05-01 02:16:48 +0300

    feat: add import grouping

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,12 +26,39 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.json',
   },
+  overrides: [
+    // override "simple-import-sort" config
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$']
+            ]
+          }
+        ]
+      }
+    }
+  ],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/react-in-jsx-scope': 'off',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
-    'max-len': ["error", { "code": 120, "ignoreStrings": true }],
+    'max-len': ['error', { 'code': 120, 'ignoreStrings': true }],
     'react/jsx-props-no-spreading': 'off',
     'react/require-default-props': 'off',
     'jsx-quotes': [2, 'prefer-single'],
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+
+import './index.css';
 
 const router = createBrowserRouter([
   {
